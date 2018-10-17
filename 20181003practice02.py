29beb7d6e462b09a08804340e0b52f43ccccb0cf
kooji1130 <43804847+kooji1130@users.noreply.github.com>
2018-10-17 14:08:32 +0800

    Update 20181003practice02.py

--- 20181003practice02.py
+++ 20181003practice02.py
@@ -23,5 +23,30 @@ for data in memberlist:
     tempbmi = round(tempbmi,10)
     if tempbmi>=27:
         print(data[0],tempbmi)
+------------------------------------------------------------------------------------------------
+name=['林姍','王志祥','李明立','黃婷婷','張文智','陳怡君','王凱群','林曉曉']
+height=[155,175,172,162,180,158,170,156]
+weight=[45,85,68,52,72,48,80,70]
+student=[]
+
+for i in range (len(name)):
+  student.append([])
+  student[i].append(name[i])
+  student[i].append(height[i])
+  student[i].append(weight[i])        
+print(student)
+
+List=[]
+print("="*30)
+print("以下同學體重過重，請平日進行飲食的調整，以維持健康的身體")
+print("姓名   BMI")
+for data in student:
+    templ=float((data[1]/100)**2)
+    tempw=int(data[2])
+    tempbmi=tempw/templ
+    tempbmi = round(tempbmi,10)
+    if tempbmi>=27:
+        print(data[0],tempbmi)
     
+
 
